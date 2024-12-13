55ba540ffc8866bb66bb6c0766fbe592e458b811
cyphersa <cyphersa@grinnell.edu>
2024-12-12 18:20:25 -0600

    Added associative array

--- src/TextSpeech.java
+++ src/TextSpeech.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 import javax.speech.Central;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerModeDesc;
-  
+
 public class TextSpeech {
   
     public static void main(String[] args)
